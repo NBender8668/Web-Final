330f6ade9c5c51e2fc2595954e367451f3c68b8f
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2020-03-05 21:07:09 -0700

    SlideShow on home page plus a css style

---
+++ Scripts/script.js
@@ -0,0 +1,47 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideFade");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlidesauto();
+
+function showSlidesauto() {
+var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("slideFade");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1;}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlidesauto, 3000); 
+}
