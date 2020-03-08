9ba4acbd89383e0f2ac79f08f9d33ec4100705db
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2020-03-07 18:38:40 -0700

    Removed Dots

--- Scripts/script.js
+++ Scripts/script.js
@@ -14,35 +14,33 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slideFade");
-  var dots = document.getElementsByClassName("dot");
+  
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].className += " active";
 }
 
 var slideIndex = 0;
 showSlidesauto();
 
 function showSlidesauto() {
-var dots = document.getElementsByClassName("dot");
+
   var slides = document.getElementsByClassName("slideFade");
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-}
+  
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1;}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].className += " active";
   setTimeout(showSlidesauto, 8000); 
 }
 
